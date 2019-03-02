9124b68b10475d59283cb4d62116d886ca7f78ac
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-01 21:04:00 -0600

    Added HTMLAlert class
    This class will return bootstrap alerts.

---
+++ CMS/HelperClasses/BootstrapAlertType.php
@@ -0,0 +1,13 @@
+<?php
+
+    abstract class BootstrapAlertType
+    {
+        const PRIMARY = "alert alert-primary";
+        const SECONDARY = "alert alert-secondary";
+        const SUCCESS = "alert alert-success";
+        const DANGER = "alert alert-danger";
+        const WARNING = "alert alert-warning";
+        const INFO = "alert alert-info";
+        const LIGHT = "alert alert-light";
+        const DARK = "alert alert-dark";
+    }
---
+++ CMS/PartialHTML/HTMLAlerts.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/HelperClasses/BootstrapAlertType.php');
+
+    class HTMLAlerts
+    {
+
+        public static function GetHTMLAlert($message, $alertType = BootstrapAlertType::DANGER){
+            return '<div class="'.$alertType.'" role="alert">' . $message . '</div>';
+        }
+
+        public static function GetHTMLAlertWCloseButton($message, $alertType = BootstrapAlertType::DANGER){
+            return '<div class="'.$alertType.'" role="alert">' . $message . '</div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+        }
+
+        public static function GetHTMLAlertWHeader($headerMessage, $message, $alertType = BootstrapAlertType::DANGER){
+            return '<div class="' . $alertType . '" role="alert"><h4 class="alert-heading">' . $headerMessage . '</h4><hr><p>' . $message . '</p></div>';
+        }
+
+        public static function GetHTMLAlertWHeaderAndCloseButton($headerMessage, $message, $alertType = BootstrapAlertType::DANGER){
+            return '<div class="' . $alertType . '" role="alert"><h4 class="alert-heading">' . $headerMessage . '</h4><hr><p>' . $message . '</p></div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+        }
+    }
