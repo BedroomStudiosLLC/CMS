ebe00ad6ab361e3627de88f424bb42b8b6fb79b2
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-01 20:25:02 -0600

    Updated DatabaseConnection
    Now using the database config file.

--- Connections/DatabaseConnection.php
+++ Connections/DatabaseConnection.php
@@ -1,9 +1,4 @@
 <?php
-
-    require_once('DatabaseCredentials.php');
-
-    echo DatabaseCredentials::$userName;
-
 	class DatabaseConnection
 	{
 		public static function GetDatabaseConnection()
@@ -12,24 +7,30 @@
 
 			if ($dbConnection === null){
 
-                $host = '127.0.0.1';
-                $db   = 'test';
-                $user = 'root';
-                $pass = '';
-                $charset = 'utf8mb4';
+                require_once($_SERVER['DOCUMENT_ROOT'].'/Config/DatabaseCredentials.php');
 
-                $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+                $dsn = DatabaseCredentials::GetDSN();
 
-                $options = [
-                    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_EMULATE_PREPARES   => false,
-                ];
+                $cred = DatabaseCredentials::GetCredentials('default');
 
-                try {
-                    $dbConnection = new PDO($dsn, $user, $pass, $options);
-                } catch (\PDOException $e) {
-                    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+                if ($cred != null) {
+
+                    $user = $cred->userName;
+                    $pass = $cred->password;
+
+                    $options = [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_EMULATE_PREPARES => false,
+                    ];
+
+                    try {
+                        $dbConnection = new PDO($dsn, $user, $pass, $options);
+                    } catch (\PDOException $e) {
+                        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+                    }
+                } else {
+                    echo 'Couldn\'t get database credentials';
                 }
 			}
 
