06c20ef78c752ba439e93a918fd03935247686a2
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-01 21:04:12 -0600

    Updated Index

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/PartialHTML/HTMLAlerts.php');
 ?>
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
 		<title>Page Title</title>
-		<?php require_once ($_SERVER['DOCUMENT_ROOT'].'/Master/HTMLHeader.php'); ?>
+		<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/Master/HTMLHeader.php'); ?>
 		<!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
 	</head>
 	<body>
 		<div class="container-fluid">
 
 			<h1>My First Heading</h1>
+
+            <?php echo HTMLAlerts::GetHTMLAlert("<b>This</b> is a simple alert", BootstrapAlertType::PRIMARY); ?>
+
 			<p>My first paragraph.</p>
 
 			<button class="btn-primary">Hi</button>
