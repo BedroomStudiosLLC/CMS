7ba0ebc2803e873882330d7caaeef4802bfaa04d
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-01 21:04:22 -0600

    Updated Template

--- Template.php
+++ Template.php
@@ -1,20 +1,23 @@
 <?php
-
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/PartialHTML/HTMLAlerts.php');
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Page Title</title>
-    <?php require_once ($_SERVER['DOCUMENT_ROOT'].'/Master/HTMLHeader.php'); ?>
-    <!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
-</head>
-<body>
-<div class="container-fluid">
+    <head>
+        <title>Page Title</title>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/Master/HTMLHeader.php'); ?>
+        <!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
+    </head>
+    <body>
+        <div class="container-fluid">
 
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
+            <h1>My First Heading</h1>
 
-    <button class="btn-primary">Hi</button>
-</div>
-</body>
+            <?php echo HTMLAlerts::GetHTMLAlert("This is a simple alert", BootstrapAlertType::PRIMARY); ?>
+
+            <p>My first paragraph.</p>
+
+            <button class="btn-primary">Hi</button>
+        </div>
+    </body>
 </html>
