e98d57bbe992582f1026b9bc85c67145e44e2373
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-01 20:26:00 -0600

    Added Template.php
    Template should be used on every new page.

---
+++ Template.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <?php require_once ($_SERVER['DOCUMENT_ROOT'].'/Master/HTMLHeader.php'); ?>
+    <!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
+</head>
+<body>
+<div class="container-fluid">
+
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+
+    <button class="btn-primary">Hi</button>
+</div>
+</body>
+</html>
