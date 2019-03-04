37961b0f162e73e2be7d166307ff51ee4df92b98
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-03 20:16:55 -0600

    Update API.php
    Fixed directory error.

--- CMS/Documentation/API.php
+++ CMS/Documentation/API.php
@@ -2,3 +2,34 @@
 
 
 //Show API calls and plugins
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/Master/HTMLHeader.php'); ?>
+        <!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
+    </head>
+    <body>
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <h1>API Calls</h1>
+
+                    <?php echo HTMLAlerts::GetHTMLAlert("This is a simple alert", BootstrapAlertType::PRIMARY); ?>
+
+                    <p>My first paragraph.</p>
+
+                    <button class="btn-primary">Hi</button>
+                </div>
+                <div class="col-3"></div>
+            </div>
+        </div>
+    </body>
+</html>
+
