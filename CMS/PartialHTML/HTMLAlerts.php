be8504e0dfefd4d6259849053da7b910266d2a9c
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-04 17:56:23 -0600

    Updated HTMLAlerts.php
    Removed optional parameter.

--- CMS/PartialHTML/HTMLAlerts.php
+++ CMS/PartialHTML/HTMLAlerts.php
@@ -5,19 +5,19 @@
     class HTMLAlerts
     {
 
-        public static function GetHTMLAlert($message, $alertType = BootstrapAlertType::DANGER){
+        public static function GetHTMLAlert($message, $alertType){
             return '<div class="'.$alertType.'" role="alert">' . $message . '</div>';
         }
 
-        public static function GetHTMLAlertWCloseButton($message, $alertType = BootstrapAlertType::DANGER){
+        public static function GetHTMLAlertWCloseButton($message, $alertType){
             return '<div class="'.$alertType.'" role="alert">' . $message . '</div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
         }
 
-        public static function GetHTMLAlertWHeader($headerMessage, $message, $alertType = BootstrapAlertType::DANGER){
+        public static function GetHTMLAlertWHeader($headerMessage, $message, $alertType){
             return '<div class="' . $alertType . '" role="alert"><h4 class="alert-heading">' . $headerMessage . '</h4><hr><p>' . $message . '</p></div>';
         }
 
-        public static function GetHTMLAlertWHeaderAndCloseButton($headerMessage, $message, $alertType = BootstrapAlertType::DANGER){
+        public static function GetHTMLAlertWHeaderAndCloseButton($headerMessage, $message, $alertType){
             return '<div class="' . $alertType . '" role="alert"><h4 class="alert-heading">' . $headerMessage . '</h4><hr><p>' . $message . '</p></div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
         }
     }
