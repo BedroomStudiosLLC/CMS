8aeb8de2410a8803d54b7b78ec605b173d8d78b8
Mark Mueller <bedroomstudiosllc@gmail.com>
2019-03-04 17:57:18 -0600

    Created API Call Documentation
    You can run API commands from this page and see their output. You can add more commands through the JSON.

--- CMS/Documentation/API.php
+++ CMS/Documentation/API.php
@@ -1,35 +1,201 @@
 <?php
 
+    //TODO create JSON file, Load JSON file
 
-//Show API calls and plugins
+    $isRunningAPICommand = false;
+
+    //Show API calls and plugins
+    if (isset($_POST['SubmitInput']))
+    {
+        $isRunningAPICommand = true;
+    }
 
 ?>
 
 
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
     <head>
         <title>Page Title</title>
-        <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/Master/HTMLHeader.php'); ?>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/Master/HTMLHeader.php'); ?>
         <!-- Bootstrap documentation: https://getbootstrap.com/docs/4.3/getting-started/introduction/ -->
+        <style>
+            .Main {
+                border-style: solid;
+                border-width: 2px;
+                border-radius: .5em;
+                border-color: #04569A;
+            }
+            .Item {
+                border-style: solid;
+                border-width: 2px;
+                border-radius: .5em;
+                background-color: #4dd0e1;
+                padding: 1em;
+                margin: 1em;
+                border-color: #04569A;
+            }
+            .Output{
+                background-color: lightgray;
+                border-radius: .5em;
+                padding: 1em;
+                margin-top: 1em;
+            }
+            .OutputValue{
+                background-color: whitesmoke;
+                border-radius: .5em;
+                padding: 1em;
+                margin-top: 2em;
+            }
+            .ButtonDisabled{
+                color: #04569A;
+                background-color: whitesmoke;
+                padding: .2em;
+            }
+            .ButtonEnabled{
+                color: whitesmoke;
+                background-color: #04569A;
+                padding: .2em;
+            }
+            .active-cyan-4 input[type=text]:focus:not([readonly]) {
+                border: 1px solid #04569A;
+                box-shadow: 0 0 0 1px #04569A;
+            }
+            .active-cyan-3 input[type=text] {
+                border: 1px solid #4dd0e1;
+                box-shadow: 0 0 0 1px #4dd0e1;
+            }
+        </style>
+        <script>
+            function showRAWHTML(APIID) {
+                if( $('#'+APIID).find('.RawHTML').val() == "true") {
+                    $(document).ready(function () {
+                        $('#'+APIID).find('.OutputValue').text($('#'+APIID).find('.OutputValue').html());
+                        var raw = $('#'+APIID).find('.ButtonEnabled');
+                        var HTML = $('#'+APIID).find('.ButtonDisabled');
+                        raw.attr('class', 'ButtonDisabled');
+                        HTML.attr('class', 'ButtonEnabled');
+                        $('#'+APIID).find('.RawHTML').val("false");
+                    });
+                }
+            }
+
+            function showHTML(APIID) {
+                if( $('#'+APIID).find('.RawHTML').val() == "false")
+                {
+                    $(document).ready( function() {
+                        $('#'+APIID).find('.OutputValue').html($('#'+APIID).find('.OutputValue').text())
+                        var raw = $('#'+APIID).find('.ButtonDisabled');
+                        var HTML = $('#'+APIID).find('.ButtonEnabled');
+                        raw.attr('class', 'ButtonEnabled');
+                        HTML.attr('class', 'ButtonDisabled');
+                        $('#'+APIID).find('.RawHTML').val("true")
+                    });
+                }
+            }
+        </script>
     </head>
     <body>
         <div class="container-fluid">
-
-            <div class="row">
+            <div class="row" style="margin-top: 3em;">
                 <div class="col-3"></div>
                 <div class="col-6">
-                    <h1>API Calls</h1>
 
-                    <?php echo HTMLAlerts::GetHTMLAlert("This is a simple alert", BootstrapAlertType::PRIMARY); ?>
+                    <h1 style="text-align: center">API Calls</h1>
 
-                    <p>My first paragraph.</p>
+                    <!--<div class="active-cyan-3 active-cyan-4 mb-4">
+                        <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+                    </div>-->
 
-                    <button class="btn-primary">Hi</button>
+                    <div class="Main" style="margin-top: 3em;">
+
+                        <?php
+                            require_once ($_SERVER['DOCUMENT_ROOT']. '/CMS/Documentation/APIHTML.php');
+
+                            echo APIHTML::GETAPIHTMLAll();
+                        ?>
+
+                    </div>
                 </div>
                 <div class="col-3"></div>
             </div>
         </div>
+        <?php
+
+        if ($isRunningAPICommand)
+        {
+            $outputValue = 'Error';
+            $failed = false;
+
+            echo '<script>window.location.hash = "'.'API'.$_POST['APINumber'].'"</script>';
+
+            $apiCall = APIHTML::GETAPI($_POST['APINumber']);
+
+            if ($apiCall == -1)
+            {
+                echo 'API Error';
+            }
+
+            switch ($apiCall['Type'])
+            {
+                case 'CONSTANT':
+                    $value = GetConstant($_POST['inputParam0']);
+                    if ($value == -1)
+                        $outputValue = 'Error: Constant not valid.';
+                    else
+                        $outputValue = $value;
+                    break;
+
+                case 'FUNCTION':
+                    if (isset($_POST['inputParam0'])) {
+
+                        $parameterArray = array();
+
+                        for ($i = 0; $i < 2; $i++) {
+                            if (isset($apiCall['PlaceHolders'][$i]['Type']))
+                            {
+                                switch ($apiCall['PlaceHolders'][$i]['Type']) {
+                                    case "CONSTANT":
+                                        $value = GetConstant($_POST['inputParam' . $i]);
+                                        if ($value == -1)
+                                        {
+                                            $outputValue = 'Error: Constant not valid.';
+                                            $failed = true;
+                                        }
+
+                                        $parameterArray[$i] = $value;
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                $parameterArray[$i] = $_POST['inputParam' . $i];
+                            }
+                        }
+
+                        if (!$failed) //Run function if hadn't failed
+                            $outputValue = call_user_func_array($apiCall['FunctionName'], $parameterArray);
+                    }
+                    break;
+            }
+
+            echo "<script>$(document).ready( function() { $('#API" . $_POST['APINumber'] . "').find('.OutputValue').html('" . $outputValue . "'); })</script>";
+
+        }
+
+        function GetConstant($constant)
+        {
+            if (defined($constant))
+            {
+                return constant($constant);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        ?>
     </body>
 </html>
 
---
+++ CMS/Documentation/APIHTML.php
@@ -0,0 +1,116 @@
+<?php
+
+APIHTML::GETAPIHTMLAll();
+
+class APIHTML
+{
+    public static function GETAPIHTMLAll()
+    {
+        require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/HelperClasses/JSONHandler.php');
+
+        $json = JSONHandler::GetConfigElement($_SERVER['DOCUMENT_ROOT'] . '/CMS/Documentation/APIJSON.php', null);
+
+        //print_r($json);
+
+        $index = 0;
+
+        $html = '';
+
+        foreach ($json as $type) { //Type ex: CMS
+            //echo PHP_EOL . "HI:";
+            foreach ($type as $APICall) { //APICall ex: Bootstrap Alert Types
+                $html .= self::GetAPIHTMLBlock($APICall, $index);
+                $index += 1;
+            }
+        }
+
+        return $html;
+    }
+
+    public static function GETAPI($indexVal)
+    {
+        require_once($_SERVER['DOCUMENT_ROOT'] . '/CMS/HelperClasses/JSONHandler.php');
+
+        $json = JSONHandler::GetConfigElement($_SERVER['DOCUMENT_ROOT'] . '/CMS/Documentation/APIJSON.php', null);
+
+        //print_r($json);
+
+        $index = 0;
+
+        $html = '';
+
+        foreach ($json as $type) { //Type ex: CMS
+            //echo PHP_EOL . "HI:";
+            foreach ($type as $APICall) { //APICall ex: Bootstrap Alert Types
+                if ($index == $indexVal){
+                    return $APICall;
+                }
+                $index += 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static function GetAPIHTMLBlock($apiCallJSON, $index)
+    {
+
+        require_once ($_SERVER['DOCUMENT_ROOT']. $apiCallJSON['URL']);
+
+        $value = '<div class="Item" id="API'.$index.'">
+                            <?php require_once ($_SERVER[\'DOCUMENT_ROOT\']. \'/CMS/HelperClasses/BootstrapAlertType.php\'); ?>
+                            <h3 style="display:inline">'.$apiCallJSON['Name'].'</h3><p style="display:inline; float: right"><b>URL:</b> '.$apiCallJSON['URL'].'</p>
+                            <br>
+                            <br>
+                            <h6>Class: '.$apiCallJSON['Class'].'</h6>
+                            <h6>Returns: '.$apiCallJSON['Returns'].'</h6>
+                            <hr><p>'.$apiCallJSON['Description'].'</p>';
+
+        if (isset($apiCallJSON['List']))
+        {
+            $value .= '<ul>';
+            foreach ($apiCallJSON['List'] as $Item)
+            {
+                $value .= '<li>'.$Item['Item'].'</li>';
+            }
+            $value .= '</ul>';
+        }
+
+                            $value .= '
+                            <h4><b>Try it out!</b></h4>
+
+                            <div class="Output">
+                                <h3 style="display: inline">Input:</h3>
+                                <br>
+                                <br>
+                                <form action="API.php" method="post">
+                                    <input type="hidden" name="APINumber" value="'.$index.'"/>
+                                    <div class="form-group row">
+                                        <div class="col-sm-5">';
+
+        $i = 0;
+        foreach ($apiCallJSON['PlaceHolders'] as $Parameter)
+        {
+            $value .= '<input type="text" class="form-control" name="inputParam'.$i.'" placeholder="'.$Parameter[$i].'">';
+            $i += 1;
+        }
+
+                                        $value .= '</div>
+                                        <div class="col-sm-2">
+                                            <input type="submit" class="form-control btn-primary btn" id="APIButton'.$index.'" value="Submit" name="SubmitInput">
+                                        </div>
+                                    </div>
+                                </form>
+                                <div><b>Syntax:</b> '.$apiCallJSON['Syntax'].'</div>
+                            </div>
+                            <div class="Output">
+                                <h3>Output:</h3>
+                                <p class="ButtonEnabled" style="float:right" onclick="showRAWHTML(\'API'.$index.'\')">Raw</p><p class="ButtonDisabled" style="float:right" onclick="showHTML(\'API'.$index.'\')">HTML</p>
+                                <input type="hidden" class="RawHTML" value="true" />
+                                <div class="OutputValue"></div>
+                            </div>
+                        </div>';
+
+        return $value;
+    }
+}
